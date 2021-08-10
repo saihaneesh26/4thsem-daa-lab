c4e8bc5bb14080130aaf8cc36d796d9f194c863a
saihaneesh26 <56464193+saihaneesh26@users.noreply.github.com>
2021-08-10 11:34:44 +0530

    Update 1bs.c

--- 1bs.c
+++ 1bs.c
@@ -3,7 +3,9 @@
 #include<time.h>
 void main()
 {
-int n=1000;
+	int k;
+	for(k=1;k<=10;k++){
+int n=10000*k;
     int a[n];
 int i,j;
     for( i=0;i<=n-1;i++)
@@ -30,6 +32,7 @@ double start = clock();
 double end = clock();
 double t = (end-start)/CLOCKS_PER_SEC;
 printf("n:%d -- total:%e\n",n,t);
+}
 }
 //gcc name.c 
 //./a.out
