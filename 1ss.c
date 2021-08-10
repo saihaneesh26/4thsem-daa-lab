de3015138d32666129593c6264feb9a38b9de93d
saihaneesh26 <56464193+saihaneesh26@users.noreply.github.com>
2021-08-10 16:06:16 +0530

    Update 1ss.c

--- 1ss.c
+++ 1ss.c
@@ -1,4 +1,3 @@
-//selection sort
 #include<stdio.h>
 #include<time.h>
 #include<stdlib.h>
@@ -6,34 +5,34 @@
 void main()
 {
     int i,k;
-   for(i=1;i<11;i++)
-   {
-        int n=10000*i;
+   for(i=1;i<10;i++)
+    {
+        int n=1000*i;
          int arr[n];
         for(k=0;k<n;k++)
         {
             int r = rand();
-            arr[k]=r;
+            arr[k]=11-k;
         }
         double start = clock();
         {
             for(int j=0;j<n-1;j++)
             {
-                 int temp = arr[j];
+                int temp = arr[j];
                 int min = j;
-                for(int k=j+1,k<n-1;k++)
+                for(int k=j+1;k<n;k++)
                 {
-                    if(arr[k]<min)
+                    if(arr[k]<arr[min])
                     {
                         min = k;
                     }
                 }
-                arr[j] = arr[k];
-                arr[k]= temp;
+                arr[j] = arr[min];
+                arr[min]= temp;
             }
         }
         double end = clock();
         double time = (end-start)/CLOCKS_PER_SEC;
-        printf("n:%d time:%lf\n",n,time);
-   }
+        printf("n:%d time:%lfs\n",n,time);
+    }
 }
